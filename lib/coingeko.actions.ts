3e3895c7ae4c8a781a79916c9e607744dc834834
Samarth Shetty <sshett03@gmail.com>
2026-01-10 21:49:04 -0500

    coins api integrated

--- lib/coingeko.actions.ts
+++ lib/coingeko.actions.ts
@@ -23,7 +23,7 @@ export async function fetcher<T>(
     const response = await fetch(url, {
         headers: {
             'Content-Type': 'application/json',
-            'x-ch-pro-api-key': API_KEY
+            'x-cg-pro-api-key': API_KEY
         } as Record<string, string>,    
         next: {revalidate}
     });
@@ -32,6 +32,6 @@ export async function fetcher<T>(
         const errorData: CoinGeckoErrorBody = await response.json();
         throw new Error(`Error fetching data from CoinGecko: ${response.status} ${errorData.error} || ${response.statusText}`);
     }
-    
+
     return response.json();
 }
