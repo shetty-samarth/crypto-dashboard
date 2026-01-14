32fa1cbfb7a63525c4473a2b1729b92ed6bdb2e7
Samarth Shetty <sshett03@gmail.com>
2026-01-13 20:10:52 -0500

    styling fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import CoinOverview from '@/components/home/CoinOverview';
 import TrendingCoins from '@/components/home/TrendingCoins';
+import { Suspense } from 'react';
 
 const page = async () => {
   return (
     <main className="main-container">
       <section className="home-grid">
-        <CoinOverview />
-        <TrendingCoins />
+        <Suspense fallback={<div>Loading Overview...</div>}>
+          <CoinOverview />
+        </Suspense>
+        <Suspense fallback={<div>Loading Trending Coins...</div>}>
+          <TrendingCoins />
+        </Suspense>
       </section>
       <section className="w-full mt-7 space-y-4">
         <p>Categories</p>
