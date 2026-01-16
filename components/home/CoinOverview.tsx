57632c7f08b1a5db92aadba3504af488c2b60102
Samarth Shetty <sshett03@gmail.com>
2026-01-15 21:41:21 -0500

    restructure error boundaries

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,36 +1,35 @@
 import { fetcher } from '@/lib/coingeko.actions';
 import Image from 'next/image';
 import { formatCurrency } from '@/lib/utils';
-import {CoinOverViewFallback} from '../fallback';
+import { CoinOverViewFallback } from '../fallback';
 
 const CoinOverview = async () => {
+  let coinData: CoinDetailsData | null = null;
   try {
-    const coinData = await fetcher<CoinDetailsData>('/coins/bitcoin', {
+    coinData = await fetcher<CoinDetailsData>('/coins/bitcoin', {
       dex_pair_format: 'symbol',
     });
 
     if (!coinData || !coinData.market_data) {
       throw new Error('Invalid coin data');
     }
-
-    return (
-      <div id="coin-overview">
-        <div className="header">
-          <Image src={coinData.image.large} alt={coinData.name} width={56} height={56} />
-          <div className="info">
-            <p>{`${coinData.name} / ${coinData.symbol.toUpperCase()}`}</p>
-            <h1>{formatCurrency(coinData.market_data.current_price.usd)}</h1>
-          </div>
-        </div>
-      </div>
-    );
   } catch (error) {
     console.error('Failed to load coin overview:', error);
 
-    return (
-      <CoinOverViewFallback />
-    );
+    return <CoinOverViewFallback />;
   }
+
+  return (
+    <div id="coin-overview">
+      <div className="header">
+        <Image src={coinData.image.large} alt={coinData.name} width={56} height={56} />
+        <div className="info">
+          <p>{`${coinData.name} / ${coinData.symbol.toUpperCase()}`}</p>
+          <h1>{formatCurrency(coinData.market_data.current_price.usd)}</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoinOverview;
