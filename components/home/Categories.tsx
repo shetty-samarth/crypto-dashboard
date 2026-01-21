df945b1660af595f4c83a21bc18f492185a6f291
Samarth Shetty <sshett03@gmail.com>
2026-01-20 19:00:04 -0500

    categories table scaffolding

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { fetcher } from "@/lib/coingeko.actions"
+import DataTable from "../DataTable";
 
-const Categories = () => {
-  return (
-    <div>Categories</div>
+const Categories = async() => {
+    const categories = await fetcher<Category[]>('/coins/categories');
+    const columns: DataTableColumn<Category>[] = [
+        {
+            header: 'Category',
+            cell: (category) => <p>{category.name}</p>,
+            cellClassName: 'category-cell',
+        },
+    ];
+  return (  
+    <div id="categories" className="custom-scrollbar">
+        <h4>Top Categories</h4>
+        <DataTable columns={columns} data={categories?.slice(0,10)} rowKey={(_, index) => index}
+        tableClassName="mt-3" />
+    </div>
   )
 }
 
