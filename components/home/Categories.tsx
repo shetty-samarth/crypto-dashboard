c041a9ae41d2a03d14f1fd89468cea49afde140d
Samarth Shetty <sshett03@gmail.com>
2026-01-20 19:00:30 -0500

    code formatting

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -1,22 +1,26 @@
-import { fetcher } from "@/lib/coingeko.actions"
-import DataTable from "../DataTable";
+import { fetcher } from '@/lib/coingeko.actions';
+import DataTable from '../DataTable';
 
-const Categories = async() => {
-    const categories = await fetcher<Category[]>('/coins/categories');
-    const columns: DataTableColumn<Category>[] = [
-        {
-            header: 'Category',
-            cell: (category) => <p>{category.name}</p>,
-            cellClassName: 'category-cell',
-        },
-    ];
-  return (  
+const Categories = async () => {
+  const categories = await fetcher<Category[]>('/coins/categories');
+  const columns: DataTableColumn<Category>[] = [
+    {
+      header: 'Category',
+      cell: (category) => <p>{category.name}</p>,
+      cellClassName: 'category-cell',
+    },
+  ];
+  return (
     <div id="categories" className="custom-scrollbar">
-        <h4>Top Categories</h4>
-        <DataTable columns={columns} data={categories?.slice(0,10)} rowKey={(_, index) => index}
-        tableClassName="mt-3" />
+      <h4>Top Categories</h4>
+      <DataTable
+        columns={columns}
+        data={categories?.slice(0, 10)}
+        rowKey={(_, index) => index}
+        tableClassName="mt-3"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
