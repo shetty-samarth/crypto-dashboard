8cd46e862acc0ce500ac72947e5ad53a122c0780
Samarth Shetty <sshett03@gmail.com>
2026-01-24 16:03:00 -0500

    price trend UI fixes

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -26,24 +26,24 @@ const Categories = async () => {
         header: 'Market Cap Change (24h)',
         cellClassName: 'market-cap-cell',
         cell: (category) => {
-        const change = category.market_cap_change_24h ?? 0;
-        const isTrendingUp = change >= 0;
+          const change = category.market_cap_change_24h ?? 0;
+          const isTrendingUp = change >= 0;
 
-        return (
+          return (
             <div className={isTrendingUp ? 'text-green-500' : 'text-red-500'}>
-          <p className='flex'>
-            {Math.abs(change).toFixed(2)}%
-            {isTrendingUp ? (
-              <TrendingUp width={16} height={16} />
-            ) : (
-              <TrendingDown width={16} height={16} />
-            )}
-          </p>
-          </div>
-        );
+              <p className="flex">
+                {Math.abs(change).toFixed(2)}%
+                {isTrendingUp ? (
+                  <TrendingUp width={16} height={16} />
+                ) : (
+                  <TrendingDown width={16} height={16} />
+                )}
+              </p>
+            </div>
+          );
+        },
       },
-    },
-    {
+      {
         header: 'Market Cap',
         cellClassName: 'market-cap-cell',
         cell: (category) => formatCurrency(category.market_cap),
