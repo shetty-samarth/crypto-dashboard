b55d8733e6c23ddd78133f622a391250eb11a9da
Samarth Shetty <sshett03@gmail.com>
2026-02-01 17:01:54 -0500

    websocket test

--- hooks/useCoinGekoWebsocket.ts
+++ hooks/useCoinGekoWebsocket.ts
@@ -1,4 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
+//Need paid plan to access WebSocket API
+const WS_BASE_URL = `${process.env.NEXT_PUBLIC_COINGECKO_WS_URL}?x_cg_pro_api_key=${process.env.NEXT_PUBLIC_COINGECKO_API_KEY}`;
 
 export const useCoinGekoWebsocket = ({coinId, poolId, liveInterval}: UseCoinGeckoWebSocketProps): UseCoinGeckoWebSocketReturn => {
     const wsRef = useRef<WebSocket | null>(null);
@@ -8,6 +10,8 @@ export const useCoinGekoWebsocket = ({coinId, poolId, liveInterval}: UseCoinGeck
     const [trade, setTrade] = useState<Trade[]>([]);
     const [ohlcv, setOhlcv] = useState<OHLCData | null>(null);
     const [isConnected, setIsConnected] = useState<boolean>(false);
+
+    useEffect(() => {}, [coinId, poolId, liveInterval]);
 
     return {
         price,
