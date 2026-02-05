cc9197d09c0968555ee497829fb1b2e1900265db
Samarth Shetty <sshett03@gmail.com>
2026-02-04 20:30:01 -0500

    coins details page

--- app/coins/[id]/page.tsx
+++ app/coins/[id]/page.tsx
@@ -1,8 +1,26 @@
+import { fetcher } from '@/lib/coingeko.actions';
 import React from 'react';
 
 const page = async ({ params }: NextPageProps) => {
   const { id } = await params;
-  return <div>Details for {id}</div>;
+  const coinData = await fetcher<CoinMarketData>(`/coins/${id}`, {
+    dex_pair_format: 'contract_address'
+  });
+  console.log('coinData:', coinData);
+  return (
+    <main id='coin-details-page'>
+        <section className='primary'>
+            <h1 className='text-3xl font-bold'>{coinData?.name}</h1>
+            <p>Trend Overview</p>
+            <p>Recent Trades</p>
+            <p>Exchange Listing</p>
+        </section>
+        <section className='secondary'>
+            <h4>Coin Details</h4>
+            <ul className='details-grid'></ul>
+        </section>
+    </main>
+  )
 };
 
 export default page;
